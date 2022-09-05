01e08053c979e2f984c010668fe40a8755d22dda
wisberry <wisberry2022@gmail.com>
2022-09-05 17:38:31 +0900

    09/05 upload

--- js/main.js
+++ js/main.js
@@ -1,42 +1,165 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-
   document.querySelector('.top_close_btn').addEventListener('click', function () {
-    this.classList.toggle('on');
+    //this.classList.toggle('on');
     document.querySelector('.TopBanner').classList.add('on');
-  })
+    document.querySelector('.MainVisual').classList.add('on');
+  });
 
-  const MAINSLIDE = new Swiper('.swiper', {
+  document.querySelector('.lang strong').addEventListener('click', function () {
+    this.classList.toggle('on');
+    document.querySelector('.lang').classList.toggle('on');
+  });
+
+  document.querySelector('.top_search strong').addEventListener('click', function () {
+    this.classList.toggle('on');
+    document.querySelector('.top_search').classList.toggle('on');
+  });
+
+
+  // scroll event
+  // const SEC = document.querySelectorAll('.action');
+  // const WT=window.innerHeight;
+
+  // window.addEventListener('scroll', () => {
+  //     let sct = window.scrollY;
+  //     SEC.forEach(ele => {
+  //         let secTop = ele.offsetTop;
+  //         let secH = ele.clientHeight;
+  //         if (sct > secTop - (WT - secH) / 2 - 200) {
+  //             ele.classList.add('on');
+  //         };
+  //         // sct > secTop - (WT-secH) / 2 - 200 ? ele.classList.add('on') : e.classList.remove('on');
+  //     });
+  // });
+
+
+
+
+  window.addEventListener('scroll', () => {
+    let SCT = window.scrollY;
+    SCT > 0
+      ? document.querySelector('.Header').classList.add('on')
+      : document.querySelector('.Header').classList.remove('on');
+
+  });
+
+  const slideDots = document.querySelectorAll('.slide_dots li');
+
+  const MAINSLIDE = new Swiper('.main_slider', {
     loop: true,
+    slideActiveClass: 'on', // .swiper-slide-active 대신 .on을 쓸거임...
     on: {
-      init: function () {
-        const current = document.querySelector('.swiper-slide-active');
-        current.classList.add('on');
-
-        document.querySelector('.main_slider_num').innerHTML = (this.realIndex + 1) + " / " + (this.slides.length - 2);
+      slideChangeTransitionEnd: function () {
+        let count = this.realIndex; // 0 1 2
+        slideDots.forEach(it => it.classList.remove('on'))
+        slideDots[count].classList.add('on');
+        document.querySelector('.main_slider_num').innerHTML = (this.realIndex + 1) + " / <span>" + (this.slides.length - 2);
       }
     }
   });
 
-  const totalSlide = document.querySelectorAll('.swiper-slide');
-  const slideDots = document.querySelectorAll('.slide_dots li');
-
-  MAINSLIDE.on('slideChangeTransitionEnd', function () {
-    const current = document.querySelector('.swiper-slide-active');
-
-    totalSlide.forEach(it => it.classList.remove('on'));
-    current.classList.add('on');
-    let count = this.realIndex;
-    slideDots.forEach(it => it.classList.remove('on'));
-    slideDots[count].classList.add('on');
-    document.querySelector('.main_slider_num').innerHTML = (this.realIndex + 1) + " / " + (this.slides.length - 2);
-  });
-
-  document.querySelector('.MainVisual .next').addEventListener('click', () => {
+  document.querySelector('.MainVisual .slide_handler .next').addEventListener('click', () => {
     MAINSLIDE.slideNext();
   });
-
-  document.querySelector('.MainVisual .prev').addEventListener('click', () => {
+  document.querySelector('.MainVisual .slide_handler .prev').addEventListener('click', () => {
     MAINSLIDE.slidePrev();
   });
+
+
+  slideDots.forEach((it, idx) => {
+    it.addEventListener('click', () => {
+      console.log(idx);
+      MAINSLIDE.slideTo(idx + 1, 600)
+    })
+  });
+
+  const PRS = new Swiper('.portfolio_right_slide', {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 30,
+  });
+
+
+  const PLS = new Swiper('.portfolio_left_slide', {
+    loop: true,
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true
+    },
+  });
+
+
+
+  document.querySelector('.Portfolio .slide_handler .next').addEventListener('click', () => {
+    PLS.slideNext();
+    PRS.slideNext();
+  });
+  document.querySelector('.Portfolio .slide_handler .prev').addEventListener('click', () => {
+    PLS.slidePrev();
+    PRS.slidePrev();
+  });
+
+  PLS.controller.control = PRS;
+  PRS.controller.control = PLS;
+
+  //출처: https://www.biew.co.kr/entry/Swiper-슬라이드-Swiper-2개-연동과-제어Controller [웹퍼블리싱 - 퍼블리싱 이야기 맑은커뮤니케이션:티스토리]
+  //centeredSlides: true,
+
+  const SCBOX = document.querySelectorAll('.Solution .content_box>div');
+
+  const SCS = new Swiper('.Solution .center_slider', {
+    loop: true,
+    // slidesPerView: 2,
+    spaceBetween: 100,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    //width: 1200,
+    slideActiveClass: 'on', // .swiper-slide-active 대신 .on을 쓸거임...
+    on: {
+      slideChangeTransitionEnd: function () {
+        let count = this.realIndex; // 0 1 2 3 4 
+        SCBOX.forEach(it => it.classList.remove('on'))
+        SCBOX[count].classList.add('on');
+        document.querySelector('.solution_slider_num').innerHTML = "0" + (this.realIndex + 1) + "<span>  / 0" + SCBOX.length;
+      }
+    }
+
+  });
+
+
+  document.querySelector('.Solution .slide_handler .next').addEventListener('click', () => {
+    SCS.slideNext();
+  });
+  document.querySelector('.Solution .slide_handler .prev').addEventListener('click', () => {
+    SCS.slidePrev();
+  });
+
+
+  const COOKIE = document.cookie;
+  console.log(COOKIE);
+  if (!COOKIE) {
+    document.querySelector('.popup').style.display = 'block';
+  }
+
+  document.querySelector('.popup button').addEventListener('click', () => {
+    document.querySelector('.popup').style.display = 'none';
+  });
+
+  function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setDate(d.getDate() + exdays);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  document.querySelector('.popup input').addEventListener('change', () => {
+    document.querySelector('.popup').style.display = 'none';
+    setCookie('user', 'week-hide', 1);
+  });
+
+
+
 });
+
+
